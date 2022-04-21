11f2a8201cb972a477317d273aa119386ee23bae
Tr00d <guillaume.faas@live.fr>
2022-04-21 18:06:05 +0200

    Remove expression bodied members

--- src/c#/password-challenge/Password/PasswordValidator.cs
+++ src/c#/password-challenge/Password/PasswordValidator.cs
@@ -4,26 +4,36 @@ namespace Password;
 
 public static class PasswordValidator
 {
-    private static bool IsValid(PasswordWithPolicy passwordWithPolicy) =>
-        passwordWithPolicy.Range
+    private static bool IsValid(PasswordWithPolicy passwordWithPolicy)
+    {
+        return passwordWithPolicy.Range
             .Contains(passwordWithPolicy
                 .Password
                 .Count(p => p == passwordWithPolicy.Letter)
             );
+    }
+        
 
-    public static int CountValidPasswords(IEnumerable<string> lines) =>
-        lines
+    public static int CountValidPasswords(IEnumerable<string> lines)
+    {
+        return lines
             .Select(line => line.ToPasswordWithPolicy())
             .Count(IsValid);
+    }
     
     private static readonly Regex PasswordRegex = new(@"(\d+)-(\d+) ([a-z]): ([a-z]+)");
-    public static IEnumerable<string> SplitToLines(this string str) => str.Split(Environment.NewLine);
+    public static IEnumerable<string> SplitToLines(this string str)
+    {
+        return str.Split(Environment.NewLine);
+    }
 
-    public static PasswordWithPolicy ToPasswordWithPolicy(this string input) =>
-        PasswordRegex.Matches(input)
+    public static PasswordWithPolicy ToPasswordWithPolicy(this string input)
+    {
+        return PasswordRegex.Matches(input)
             .ToList()
             .Select(ToPasswordWithPolicy)
             .Single();
+    }
 
     private static IEnumerable<int> ToRange(Match match)
     {
@@ -33,11 +43,13 @@ public static class PasswordValidator
         return Enumerable.Range(start, end - start + 1);
     }
 
-    private static PasswordWithPolicy ToPasswordWithPolicy(Match match) =>
-        new PasswordWithPolicy()
+    private static PasswordWithPolicy ToPasswordWithPolicy(Match match)
+    {
+        return new PasswordWithPolicy()
         {
             Password = match.Groups[4].Value,
             Range = ToRange(match),
             Letter = match.Groups[3].Value.First()
         };
+    }
 }
